6258eff2800a14c4aec0f93925bd864495dafa39
Daniel Levin <sporks@gmail.com>
2017-02-14 11:17:32 -0500

    created schema

---
+++ server/controller/dbSchema.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let trainSchema = new Schema({
+  row: {type: Number, index: true},
+  timeNow: {type: Date, default: Date.now},
+  origin: String,
+  trip: String,
+  destination: String,
+  scheduled: Date,
+  lateness: Number,
+  track: Number,
+  status: Number
+});
+
+module.exports = mongoose.model('trainSchema', trainSchema);
