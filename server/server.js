1d367ac671934e6dc62dfad3312ab462af42d167
Daniel Levin <sporks@gmail.com>
2017-02-15 13:26:57 -0500

    started u[ basic webserver

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 'use strict'
 
 const express = require('express');
-const app = express();
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
 const path = require('path');
 const request = require('request');
 const getSchedule = require('./middleware/getSchedule');
 const dbHelper = require('./controllers/dbHelper');
 const mongoose = require('mongoose');
-//TODO Convert from middleware to promises
-mongoose.Promise = Promise;
+const app = express();
 
+const compiler = webpack(webpackConfig);
+
+//Use native ES6 Promises with mongoose
+mongoose.Promise = Promise;
 mongoose.connect( 'mongodb://mbta:mbta@ds153239.mlab.com:53239/mbta');
 const db = mongoose.connection;
 
@@ -17,6 +22,18 @@ db.on( 'error', console.error.bind( console, 'connection error:' ) );
 db.once( 'open', function () {
 	console.log( "your db is open" );
 } );
+
+app.use(express.static(__dirname + './../www'));
+
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
 
 
 app.get('/schedule', getSchedule.checkCache, getSchedule.download, function (req, res) {
